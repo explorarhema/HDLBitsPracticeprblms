f7224b92e573eba5dd588107521068f887b29be3
Hema <145772068+explorarhema@users.noreply.github.com>
2024-07-02 19:03:52 +0530

    Update Notgate.v

--- Notgate.v
+++ Notgate.v
@@ -1,1 +1,4 @@
-
+module top_module( input in, output out );
+  assign out = !in; //bitwise-NOT (~) and logical-NOT (!)
+  // input in and input wire a are same, here by default in and out are considered as wires
+endmodule
